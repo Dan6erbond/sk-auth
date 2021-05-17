4ad637dd36b236f9e3974fa04eae99fa82226bed
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-17 18:47:18 +0200

    :tada: Initialize NPM project and install dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2019,
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["off", { argsIgnorePattern: "^_" }],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+};
