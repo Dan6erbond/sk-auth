768b7b9fef725f24ebfe71b9d669630fbdedf087
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-20 19:59:16 +0200

    🔧 Update example app env variables

--- example-app/src/global.d.ts
+++ example-app/src/global.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="@sveltejs/kit" />
+
+interface ImportMetaEnv {
+  VITE_GOOGLE_OAUTH_CLIENT_ID: string;
+  VITE_GOOGLE_OAUTH_CLIENT_SECRET: string;
+  VITE_FACEBOOK_OAUTH_CLIENT_ID: string;
+  VITE_FACEBOOK_OAUTH_CLIENT_SECRET: string;
+  VITE_TWITTER_API_KEY: string;
+  VITE_TWITTER_API_SECRET: string;
+  VITE_REDDIT_API_KEY: string;
+  VITE_REDDIT_API_SECRET: string;
+}
