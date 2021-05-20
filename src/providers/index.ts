e13255116c3adaa2626a43396354fa7eec8480d7
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-20 20:00:12 +0200

    ♻️ Export all providers from `/providers` module

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,2 +1,6 @@
 export { Provider } from "./base";
 export { GoogleOAuthProvider } from "./google";
+export { TwitterAuthProvider } from "./twitter";
+export { FacebookAuthProvider } from "./facebook";
+export { OAuth2Provider } from "./oauth2";
+export { RedditOAuthProvider } from "./reddit";
