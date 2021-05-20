5ed4607fe56c0a607528055d7ec2836fb3ad05d4
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-20 12:25:44 +0200

    💄 Add TWCSS and base styles with fonts Inter/Fira Mono

---
+++ example-app/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import "../app.postcss";
+</script>
+
+<main>
+  <slot />
+</main>
+
+<style lang="postcss">
+  main {
+    @apply flex flex-1 flex-col p-4 w-full max-w-5xl my-0 mx-auto box-border mt-20;
+  }
+</style>
--- example-app/src/routes/index.svelte
+++ example-app/src/routes/index.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
