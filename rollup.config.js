a067b10fb8f49711516c360171a0ab312a60a7bc
RaviAnand Mohabir <moravrav@gmail.com>
2021-05-23 15:44:36 +0200

    🏷️ Export types as individual files to avoid protected access errors

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import multiInput from "rollup-plugin-multi-input";
-import dts from "rollup-plugin-dts";
 
 // this override is needed because Module format cjs does not support top-level await
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -33,20 +32,5 @@ export default [
       }),
     ],
     external: Object.keys(globals),
-  },
-  {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
-  {
-    input: "dist/types/client/index.d.ts",
-    output: [{ file: "dist/client/index.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
-  {
-    input: "dist/types/providers/index.d.ts",
-    output: [{ file: "dist/providers/index.d.ts", format: "es" }],
-    plugins: [dts()],
   },
 ];
