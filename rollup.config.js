6f53ada391b5367824e2c8fc6d67be462a137567
Dan6erbond <moravrav@gmail.com>
2021-05-25 09:57:29 +0200

    👷 Emit Typescript declaration files with Rollup Plugin Typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import multiInput from "rollup-plugin-multi-input";
 import packageJson from "./package.json";
 import esbuild from "rollup-plugin-esbuild";
+import typescript from "@rollup/plugin-typescript";
 
 const globals = {
   ...packageJson.dependencies,
@@ -15,7 +16,14 @@ export default [
       sourcemap: true,
       format: "cjs",
     },
-    plugins: [esbuild(), multiInput()],
+    plugins: [
+      esbuild(),
+      multiInput(),
+      typescript({
+        emitDeclarationOnly: true,
+        sourceMap: false,
+      }),
+    ],
     external: [
       ...Object.keys(globals),
       "@sveltejs/kit/assets/runtime/app/navigation",
