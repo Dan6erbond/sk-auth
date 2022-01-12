ac77764b3d42da39a2b85325aedb0767f20ca90a
Dan6erbond <moravrav@gmail.com>
2022-01-12 20:28:20 +0100

    [FIX] Fix Client Functions and Add Base Path Option (#63)
    Fixes #54.

--- src/client/signOut.ts
+++ src/client/signOut.ts
@@ -1,14 +1,18 @@
 /* import { session as session$ } from "$app/stores"; */
 
-export async function signOut() {
-  let res = await fetch("/api/auth/signout", { method: "POST" });
+import type { ClientRequestConfig } from "./types";
+
+export async function signOut(config?: ClientRequestConfig) {
+  let res = await fetch(mergePath(["/api/auth", config?.basePath ?? null], "signout"), {
+    method: "POST",
+  });
   const { signout } = await res.json();
 
   if (!signout) {
     throw new Error("Sign out not successful!");
   }
 
-  res = await fetch("/api/auth/session");
+  res = await fetch(mergePath(["/api/auth", config?.basePath ?? null], "session"));
   const session = await res.json();
 
   return session;
