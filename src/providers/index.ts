52ca51f41d8a91a1213fbfa6a3e65a7ba1dc1870
Christopher McCulloh <cmcculloh@salesforce.com>
2022-04-20 12:44:08 -0400

    adds Okta to providers/index.ts

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -9,5 +9,7 @@ export type { FacebookProfile, FacebookTokens } from "./facebook";
 export { OAuth2BaseProvider } from "./oauth2.base";
 export type { ProfileCallback } from "./oauth2.base";
 export { OAuth2Provider } from "./oauth2";
+export { OktaOAuth2Provider } from "./okta";
+export type { OktaProfile, OktaTokens, OktaAddress } from "./okta";
 export { RedditOAuth2Provider } from "./reddit";
 export type { RedditProfile, RedditTokens } from "./reddit";
